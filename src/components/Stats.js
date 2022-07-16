159e7b2cb153e741057ffc64a97c6cc98863cb0b
Rancor38 <zak.rancor@hotmail.com>
2022-07-15 18:20:38 -0700

    Added state into Stats js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Stats = () => {
+
+    const [monster, setMonster] = useState(null);
+
+    useEffect (() => {}, []);
+
     return (
         <div className="details-container">
         <img
