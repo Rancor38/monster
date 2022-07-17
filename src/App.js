3a54ee16f2c920047cb995c89e12a3807edbab46
Rancor38 <zak.rancor@hotmail.com>
2022-07-16 18:17:52 -0700

    Added all content into one superblock on Appjs, to keep the links from breaking and run in line with initial proposal

--- src/App.js
+++ src/App.js
@@ -13,33 +13,31 @@ const App = () => {
       <Header />
 <main>
     <Routes>
-      <Route path='/' element={ <Cards 
+      <Route path='/' element={ 
+      <>
+
+      <Cards 
       url={'?document__slug=wotc-srd'}
       />
-      }/>
-      <Route path='/2' element={ <Cards 
+      <Cards 
       url={'?document__slug=wotc-srd&page=2'}
       />
-      }/>
-      <Route path='/3' element={ <Cards 
+      <Cards 
       url={'?document__slug=wotc-srd&page=3'}
       />
-      }/>
-      <Route path='/4' element={ <Cards 
+      <Cards 
       url={'?document__slug=wotc-srd&page=4'}
       />
-      }/>
-      <Route path='/5' element={ <Cards 
+      <Cards 
       url={'?document__slug=wotc-srd&page=5'}
       />
-      }/>
-      <Route path='/6' element={ <Cards 
+      <Cards 
       url={'?document__slug=wotc-srd&page=6'}
       />
-      }/>
-      <Route path='/7' element={ <Cards 
+      <Cards 
       url={'?document__slug=wotc-srd&page=7'}
       />
+      </>
       }/>
       <Route path='stats/:name' element={<Stats /> }/>
     </Routes>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,7 @@ const Footer = () => {
     return (
         <header>
             <h1>
-                <Link to='/'>1</Link>
-                ... 
-                <Link to='/2'>2</Link> 
-                ... 
-                <Link to='/3'>3</Link> 
-                ... 
-                <Link to='/4'>4</Link> 
-                ... 
-                <Link to='/5'>5</Link> 
-                ... 
-                <Link to='/6'>6</Link> 
-                ... 
-                <Link to='/7'>7</Link> 
+                Footer
             </h1>
 
         </header>
