bba12dd5018a6ceaeb9312b52e0711f6d8066e33
Rancor38 <zak.rancor@hotmail.com>
2022-07-19 11:53:58 -0700

    Finishing MVP

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import dragon from '../dragon.svg'
 
 const About = () => {
     return (
-       <>
+       <div className='detcontainer'>
         <h1 className='card-title'>Welcome to Monsters,</h1>
-        <img className='details-image'
-        src={dragon} alt="dragon"/>
         <main className='details-container'>
         <p className='details'>This application is fairly straightforward. Save the link and you can use it to browse all of the monsters in Dungeons & Dragons that are available within the System Reference Document. I'll be adding other feature to it soon to make it easier to browse the app.</p>
         </main>
-        </>
+        </div>
     );
 };
 
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -34,8 +34,9 @@ const Stats = () => {
       }
 
     return (
-        <div className="detcontainer">
-          <h2>{monst.name}</h2>
+        <>
+            <div className="detcontainer">
+          <h2 className='center-title'>{monst.name}</h2>
           <h3>{monst.size} {monst.type}, {monst.alignment}</h3>
           <Breakline/>
           <h4 className='stat-left'>Armor Class {monst.armor_class} ({monst.armor_desc})</h4>
@@ -44,7 +45,7 @@ const Stats = () => {
            Fly ({monst.speed.fly}) Swim ({monst.speed.swim}) Climb ({monst.speed.climb}) </h4>
           <Breakline/>
           {/* COME BACK AND FIX SPEED */}
-          <ul className='stats'>
+          <ul className='ability-scores'>
             {/* Ability Score */}
             <li className='stat'>Strength {monst.strength}
                 {/* Modifier */}
@@ -69,21 +70,22 @@ const Stats = () => {
           <Breakline/>
           <h4 className='stat-left'>Challenge {monst.challenge_rating}</h4>
           <ul className='stats'>
-            <li className='stat'>Damage Vulnerabilities ({monst.damage_vulnerabilities})</li>
-            <li className='stat'>Damage Resistances ({monst.damage_resistances})</li>
-            <li className='stat'>Damage Immunities ({monst.damage_immunities})</li>
-            <li className='stat'>Condition Immunities ({monst.condition_immunities})</li>
-            <li className='stat'>Senses ({monst.senses})</li>
-            <li className='stat'>Languages ({monst.languages})</li>
+            <li>Damage Vulnerabilities ({monst.damage_vulnerabilities})</li>
+            <li>Damage Resistances ({monst.damage_resistances})</li>
+            <li>Damage Immunities ({monst.damage_immunities})</li>
+            <li>Condition Immunities ({monst.condition_immunities})</li>
+            <li>Senses ({monst.senses})</li>
+            <li>Languages ({monst.languages})</li>
           </ul>
           <h4 className='stat-left'>Profieciency Bonus {Math.ceil(((monst.challenge_rating)/4)+1) || 2}</h4>
 
           <Breakline/>         
           <h3 className='stat-center'>Actions</h3>
           <b>Attacks</b>
-          <p>Attacks with which the {monst.name.toLowerCase()} is proficient are +{(Math.ceil(((monst.challenge_rating)/4)+1) || 2)+5} to hit.</p>
-          <p>Abilities that trigger saving throws are DC {(Math.ceil(((monst.challenge_rating)/4)+1) || 2)+13} to save.</p>
-          <b>Special Abilities</b>
+          <p>Attacks with which the {monst.name.toLowerCase()} is proficient should be +{(Math.ceil(((monst.challenge_rating)/4)+1) || 2)+5} to hit.</p>
+          <p>Abilities that trigger saving throws should be DC {(Math.ceil(((monst.challenge_rating)/4)+1) || 2)+13} to save.</p>
+          <p>The {monst.name.toLocaleLowerCase()} is a CR {monst.challenge_rating} creature and should deal roughly {Math.floor(5.5*monst.challenge_rating) || 3} ({Math.floor(2*monst.challenge_rating) || 1}d6) damage per round.</p>
+          {/* <b>Special Abilities</b> */}
           {/* <b>{monst.actions[0].name}</b>
           <p className='action'>{monst.actions[0].desc}</p>
           <b>{monst.actions[1].name}</b>
@@ -94,14 +96,15 @@ const Stats = () => {
           <p className='action'>{monst.actions[3].desc}</p> */}
           {/* FIND A WAY TO PRINT WITHOUT IT BREAKING */}
           <Breakline/>
-          <h4 className='stat-center'><a
+          <h4 className='stat-center'><a className='read-more'
             href={"https://www.dndbeyond.com/monsters/" + monster[0].slug}
             target="_blank"
             rel="noopener noreferrer"
           >
             Read More
           </a></h4>
-      </div>
+          </div>
+      </>
     );
 };
 
