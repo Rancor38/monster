3d2e163fe40535f5d216955f92e154f5f76707aa
Rancor38 <zak.rancor@hotmail.com>
2022-07-21 23:12:55 -0700

    Added github link in about

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import github from '../github.png'
 
 const About = () => {
     return (
@@ -6,7 +7,16 @@ const About = () => {
         <h1 className='card-title'>Welcome to Monsters,</h1>
         <main className='details-container'>
         <p className='details'>This application is fairly straightforward. Save the link and you can use it to browse all of the monsters in Dungeons & Dragons that are available within the System Reference Document. I'll be adding other feature to it soon to make it easier to browse the app.</p>
+        
         </main>
+        <h4 className='stat-center'><a className='read-more'
+            href={"https://www.dndbeyond.com/monsters/"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View the Project on GitHub <img className='github'
+        src={github} alt="github"/>
+          </a></h4>
         </div>
     );
 };
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import dot from '../3-dots-bounce.svg'
 import Breakline from './Breakline';
+import { Link } from 'react-router-dom'
 
 const Stats = () => {
 
