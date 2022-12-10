8f238d9e324c97580cae0cddd7396ad4fc99a2fb
Rancor38 <zak.rancor@hotmail.com>
2022-12-10 02:35:59 -0800

    made header motion divs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Header = () => {
-    return (
-        <>
-        <h1>Monster Finder</h1>
-        <header>    
-            <h2>
-                <Link to='/'>Find Monsters</Link> 
-                <Link to='/about'>About</Link>
-            </h2>
-            
-        </header>
-        </>
-    );
+  return (
+    <>
+      <h1>Monster Finder 5E</h1>
+      <header>
+        <motion.h2 whileHover={{ scale: 1.05 }} whileTap={{ opacity: 0 }}>
+          <Link to="/">Find Monsters</Link>
+        </motion.h2>
+        {/* <FilterForm /> */}
+        <motion.h2 whileHover={{ scale: 1.05 }} whileTap={{ opacity: 0 }}>
+          <Link to="/about">About</Link>
+        </motion.h2>
+      </header>
+    </>
+  );
 };
 
 export default Header;
